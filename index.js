1197a5f96a2174bfa71c56fee15edc390b109fae
varunbharathi <43110919+varunbharathi@users.noreply.github.com>
2022-02-25 05:16:52 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express=require("express"); 
+const PORT=process.env.PORT || 3000;
+const app= express();        //binds the express module to 'app'
+var cors = require('cors');
+var bodyParser = require('body-parser');
+
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+const serviceAccount = require('./cargo-monitoring-7426f-7493f9debf21.json');
+const { time } = require("console");
+initializeApp({
+  credential: cert(serviceAccount)
+});
+const db = getFirestore();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/send",(req,res)=>{
+    
+    var now=new Date();
+    var timeNow= now.getTime();
+
+const data= {
+    timestamp: timeNow,
+    temperature: Number(req.body.temprature),
+    humidity: Number(req.body.humidity),
+    coordinates: req.body.coordinates
+}
+
+db.collection('cargos').doc(`${req.body.email}`).collection(`${req.body.cid}`).doc(`${timeNow}`).set(data).then(()=>
+{
+    console.log("Data entered Successfully");
+    res.send("Success!");
+}).catch((e)=>{
+    console.log("error occured", e);
+});
+
+
+
+})
+
+
+app.listen(PORT, function(){
+        console.log("SERVER STARTED ON localhost:3000");     
+})
