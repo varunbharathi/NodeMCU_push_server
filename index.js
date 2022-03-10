17735d896b542d98c51b98038e856c11974a012a
varunbharathi <43110919+varunbharathi@users.noreply.github.com>
2022-03-10 21:51:16 +0530

    IST time Converted

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 const express=require("express"); 
 const date = require('date-and-time');
 const utcToIndiantime = require('utc-to-indiantime');
-const nodemailer=require('nodemailer');
+const sgMail = require('@sendgrid/mail');
 
+const API_KEY="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";
 
-const transporter = nodemailer.createTransport({
-    service: "hotmail",
-    auth:{
-        user: "cargomonitoringalertsystem@hotmail.com",
-        pass: ")/)*uW8YMbD7&-("
-    }
-});
-
-
+sgMail.setApiKey(API_KEY);
 
 
 const PORT=process.env.PORT || 3000;
@@ -26,22 +19,23 @@ const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestor
 const serviceAccount = require('./cargo-monitoring-7426f-7493f9debf21.json');
 const { time } = require("console");
 const { syncBuiltinESMExports } = require("module");
+const { text } = require("body-parser");
 initializeApp({
   credential: cert(serviceAccount)
 });
 const db = getFirestore();
-
+id=new Date();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/send",(req,res)=>{
     
-   let nowIdutc= new Date();
-   let nowIdist=utcToIndiantime(nowIdutc); 
    
-  let nowist=String( date.format(nowIdist, 'YYYY/MM/DD HH:mm:ss'));
-   
+    var time=new Date().toLocaleString('en-US', {timeZone: 'Asia/Kolkata', hour12: false})
+    time=time.replace(",","");
+
+
   let coordinates=req.body.coordinates;
   let latLong=coordinates.split(",");
 
@@ -104,7 +98,7 @@ app.post("/send",(req,res)=>{
    
 
     const data= {
-        timestamp: nowist,
+        timestamp: time,
         temperature: Number(req.body.temperature),
         humidity: Number(req.body.humidity),
         latitude: latLong[0],
@@ -115,7 +109,7 @@ app.post("/send",(req,res)=>{
 
 var status=sendEmail(req.body.email,req.body.cid, req.body.temperature, req.body.humidity);
 
-db.collection('cargos').doc(`${req.body.cid}`).collection(`data`).doc(`${nowIdist}`).set(data).then(()=>
+db.collection('cargos').doc(`${req.body.cid}`).collection(`data`).doc(`${id}`).set(data).then(()=>
 {
     console.log("Data entered Successfully");
     res.send("Success!");
@@ -172,25 +166,19 @@ if(isThpresent)
     if(humidity> thHumidity || temperature > thTemperature)
     {
         console.log("In here!");
-var options={
-from: "CargoMonitoringAlert@outlook.com",
-to: email,
-subject: `Alert on ${cargo}`,
-text: "Set Threshold Cross Alert! Please Check Your Cargo Status."
-        }
-    
-    
-        transporter.sendMail(options, function(err, info)
-        {
-            if(err)
-            {
-                console.log(err);
-                return;
-            }
-            console.log('Message sent: ' + info.response);
-        });
-    
-    
+
+        var message= {
+            to: email,
+            from: 'cargomonitoringalertsystem@hotmail.com',
+            subject: `Alert of ${cargo} status`,
+            text: "Check your cargo Status threshold crossed!",
+            html: `<h1>Check your cargo Status threshold crossed!</h1>
+            <p>Click the link below for detailed information `
+        };
+
+        sgMail.send(message).then(res => console.log("email sent..."))
+        .catch(err=> console.log(err.message));
+
     }
    
 }
