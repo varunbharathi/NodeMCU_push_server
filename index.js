0b9287b30b7f2e02114425e94bee539a1389d821
varunbharathi <43110919+varunbharathi@users.noreply.github.com>
2022-03-10 19:00:20 +0530

    mailing System

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express=require("express"); 
 const date = require('date-and-time');
 const utcToIndiantime = require('utc-to-indiantime');
+const nodemailer=require('nodemailer');
+
+
+const transporter = nodemailer.createTransport({
+    service: "hotmail",
+    auth:{
+        user: "cargomonitoringalertsystem@hotmail.com",
+        pass: ")/)*uW8YMbD7&-("
+    }
+});
+
+
+
+
 const PORT=process.env.PORT || 3000;
+
 const app= express();        //binds the express module to 'app'
 var cors = require('cors');
 var bodyParser = require('body-parser');
@@ -10,6 +25,7 @@ const { initializeApp, applicationDefault, cert } = require('firebase-admin/app'
 const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
 const serviceAccount = require('./cargo-monitoring-7426f-7493f9debf21.json');
 const { time } = require("console");
+const { syncBuiltinESMExports } = require("module");
 initializeApp({
   credential: cert(serviceAccount)
 });
@@ -29,26 +45,75 @@ app.post("/send",(req,res)=>{
   let coordinates=req.body.coordinates;
   let latLong=coordinates.split(",");
 
+ var docRef = db.collection("cargos").doc(`${req.body.cid}`);
+ var ele=docRef.get().then((doc) => {
+    if (doc.data().humiThreshold || doc.data().tempThreshold) {
+
+        
+        const owner={
+            owner:`${req.body.email}`,
+            humiThreshold:doc.data().humiThreshold,
+            tempThreshold:doc.data().tempThreshold
+        }
+        
+        db.collection('cargos').doc(`${req.body.cid}`).set(owner).then(()=>
+        {
+         console.log("owner field added successfully")
+        }).catch((e)=>{
+            console.log("Error Occured while setting owner",e);
+        });
+        
+
+        return
+       
+    } else {
+        // doc.data() will be undefined in this case
+        const owner={
+            owner:`${req.body.email}`,
+            humiThreshold:"None",
+            tempThreshold: "None"
+        }
+        
+        db.collection('cargos').doc(`${req.body.cid}`).set(owner).then(()=>
+        {
+         console.log("owner field added successfully")
+        }).catch((e)=>{
+            console.log("Error Occured while setting owner",e);
+        });
+        return
+    }
+    }).catch((error) => {
+
+        const owner={
+            owner:`${req.body.email}`,
+            humiThreshold:"None",
+            tempThreshold: "None"
+        }
+        
+        db.collection('cargos').doc(`${req.body.cid}`).set(owner).then(()=>
+        {
+         console.log("owner field added successfully")
+        }).catch((e)=>{
+            console.log("Error Occured while setting owner",e);
+        });
+        
+        console.log("Error getting document:", error);
+        return
+    });
+
+   
+
+    const data= {
+        timestamp: nowist,
+        temperature: Number(req.body.temperature),
+        humidity: Number(req.body.humidity),
+        latitude: latLong[0],
+        longitude:latLong[1]
+    }
 
 
-const data= {
-    timestamp: nowist,
-    temperature: Number(req.body.temperature),
-    humidity: Number(req.body.humidity),
-    latitude: latLong[0],
-    longitude:latLong[1]
-}
 
-const owner={
-    owner:`${req.body.email}`
-}
-
-db.collection('cargos').doc(`${req.body.cid}`).set(owner).then(()=>
-{
- console.log("owner field added successfully")
-}).catch((e)=>{
-    console.log("Error Occured while setting owner",e);
-});
+var status=sendEmail(req.body.email,req.body.cid, req.body.temperature, req.body.humidity);
 
 db.collection('cargos').doc(`${req.body.cid}`).collection(`data`).doc(`${nowIdist}`).set(data).then(()=>
 {
@@ -72,3 +137,62 @@ app.get("/",(req,res)=>{
 app.listen(PORT, function(){
         console.log("SERVER STARTED ON localhost:3000");     
 })
+
+
+async function sendEmail(email, cargo, temperature, humidity)
+{
+
+    
+
+    var thTemperature, thHumidity;
+    var docRef = db.collection("cargos").doc(`${cargo}`);
+    var isThpresent=false;
+
+   await docRef.get().then((doc) => {
+        if (doc.data().humiThreshold || doc.data().tempThreshold) {
+
+        
+            thTemperature=doc.data().humiThreshold;
+            thHumidity= doc.data().tempThreshold;
+             isThpresent=true;
+            
+            }
+           
+         else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+        }
+    }).catch((error) => {
+        console.log("Error getting document:", error);
+    });
+
+if(isThpresent)
+{
+      
+    if(humidity> thHumidity || temperature > thTemperature)
+    {
+        console.log("In here!");
+var options={
+from: "CargoMonitoringAlert@outlook.com",
+to: email,
+subject: `Alert on ${cargo}`,
+text: "Set Threshold Cross Alert! Please Check Your Cargo Status."
+        }
+    
+    
+        transporter.sendMail(options, function(err, info)
+        {
+            if(err)
+            {
+                console.log(err);
+                return;
+            }
+            console.log('Message sent: ' + info.response);
+        });
+    
+    
+    }
+   
+}
+}
+
